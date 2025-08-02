cd079bdf860692a3f761060050e2f701e3ec6fe9
marve01-hub <parteek.marve09@gmail.com>
2025-08-02 12:12:22 +0530

    SWAP BY POINTERS

--- POINTERS/SwapFunctionPOINTERS.cpp
+++ POINTERS/SwapFunctionPOINTERS.cpp
@@ -1,15 +1,15 @@
 #include<iostream>
 using namespace std;
-void swap(int& x, int& y){
-    int temp = x;
-    x = y;
-    y = temp;
+void swap(int* a, int* b){
+    int temp = *a;
+    *a = *b;
+    *b = temp;
 }
 int main(){
     int x = 22;
     int y = 12;
     cout<<x<<" "<<y<<endl;
-    swap(x,y);
+    swap(&x,&y);
     cout<<x<<" "<<y<<endl;
 
 }
