160269266cfdb088861ed4340e0bb5caf84da316
marve01-hub <parteek.marve09@gmail.com>
2025-08-02 23:50:23 +0530

    good one

---
+++ RECURSION/printSumfrom1ToN.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+void sum(int n, int s){
+    if(n==0){
+        cout<<s<<endl;
+        return;
+    } 
+    sum(n-1,s+n);
+    
+}
+
+int main(){
+    int n;
+    cout<<"Enter n : ";
+    cin>>n;
+    sum(n,0);
+}
