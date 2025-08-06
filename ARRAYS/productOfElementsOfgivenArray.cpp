d1e51025e98698195f3935aecd0756c552e9bb55
marve01-hub <parteek.marve09@gmail.com>
2025-08-06 23:12:09 +0530

    size and no of element

--- ARRAYS/productOfElementsOfgivenArray.cpp
+++ ARRAYS/productOfElementsOfgivenArray.cpp
@@ -1,13 +1,13 @@
 #include<iostream>
 using namespace std;
 int main(){
-   int arr[] = {2,5,3,6,7,5,1};
+   int arr[] = {23,2};
    int n = sizeof(arr)/4;
-   int sum = 0;
+   int product = 1;
    for(int i = 0; i<n;i++){
-       sum += arr[i];   
+       product *= arr[i];   
    }
-   cout<<sum;
+   cout<<product;
 
 //    cout<<sizeof(arr); // total memory used 
 //    cout<<sizeof(arr)/4; // know no. of elements
