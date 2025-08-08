3a7b4ae1e4c95a209d88b3d6d7bdbeeef38dc7f2
marve01-hub <parteek.marve09@gmail.com>
2025-08-08 19:48:03 +0530

    **CONCEPT

--- ARRAYS/MaxInArray.cpp
+++ ARRAYS/MaxInArray.cpp
@@ -2,10 +2,10 @@
 using namespace std;
 int main(){
    int arr[] = {2,5,3,64,7,51,1};
-   int n = sizeof(arr)/4;
-   int mx = arr[0];
+   int n = sizeof(arr)/4; // no of integers
+   int mx = arr[0];       //started from 0th index
    for(int i = 0; i<n;i++){
-       if(arr[i]>mx) mx = arr[i]; 
+       if(arr[i]>mx) mx = arr[i]; // *********
    }
    cout<<mx;
 }
