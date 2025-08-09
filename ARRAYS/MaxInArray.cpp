ac536a8a8c6d2823ee5673c0b35126b6304ce0d5
marve01-hub <parteek.marve09@gmail.com>
2025-08-09 19:43:15 +0530

    *CONCEPT

--- ARRAYS/MaxInArray.cpp
+++ ARRAYS/MaxInArray.cpp
@@ -1,9 +1,9 @@
 #include<iostream>
 using namespace std;
 int main(){
-   int arr[] = {2,5,3,64,7,51,1};
+   int arr[] = {-2,-5,-3,-4};
    int n = sizeof(arr)/4; // no of integers
-   int mx = arr[0];       //started from 0th index
+   int mx = INT16_MIN;    // smallest value 
    for(int i = 0; i<n;i++){
     //    if(arr[i]>mx) mx = arr[i]; // *********
     mx = max(mx,arr[i]);
