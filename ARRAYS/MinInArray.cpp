ba834c297a753a677c69e4f09ecbedf0d179500a
marve01-hub <parteek.marve09@gmail.com>
2025-08-09 19:50:58 +0530

    *CONCEPT

--- ARRAYS/MinInArray.cpp
+++ ARRAYS/MinInArray.cpp
@@ -1,9 +1,9 @@
 #include<iostream>
 using namespace std;
 int main(){
-   int arr[] = {2,5,3,64,7,51,1};
+   int arr[] = {2,5,3,64,7,51};
    int n = sizeof(arr)/4; // no of integers
-   int mn = arr[0];       //started from 0th index
+   int mn = INT16_MAX;       //started from 0th index
    for(int i = 0; i<n;i++){
     //    if(arr[i]<mx) mx = arr[i]; // *********
     mn = min(mn,arr[i]);
