19b578ff48604da821e1912bb733f53d17157098
marve01-hub <parteek.marve09@gmail.com>
2025-08-11 21:35:14 +0530

    *CONCEPT

--- ARRAYS/SquaresOFAllNotillN.cpp
+++ ARRAYS/SquaresOFAllNotillN.cpp
@@ -4,9 +4,12 @@ int main(){
     int n;
     cin >> n;
     int arr[n];
-    for(int i = 1; i<=n;i++){
-        arr[i] = (i+1)*(i+1)
+    for(int i = 0; i<n;i++){  //  STORING ELEMENTS IN ARRAY
+        arr[i] = (i+1)*(i+1);
     }
-
+    for(int i = 0;i < n;i++){
+        cout<<arr[i]<<" ";
+    }
+    
     
 }
