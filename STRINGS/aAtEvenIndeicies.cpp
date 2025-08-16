a6b30bb03496bfcec3bbe2c6e8ba658cf1203cce
marve01-hub <parteek.marve09@gmail.com>
2025-08-16 13:06:31 +0530

    a at even

---
+++ STRINGS/aAtEvenIndeicies.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+   
+    string s = "parteek marve";
+    int n = s.length();
+    for(int i = 0; i<n;i++){
+        if(i%2==0){
+            s[i] = 'a';
+        }
+    }
+   cout<<s<<" ";
+    
+}
