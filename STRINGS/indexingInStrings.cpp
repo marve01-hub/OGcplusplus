ff7d9d0f29a3b5a80b8ab0f50311703abc4e16b6
marve01-hub <parteek.marve09@gmail.com>
2025-08-16 12:16:54 +0530

    indexing

---
+++ STRINGS/indexingInStrings.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+    string str = "Parteek";
+    // cout<<str[0];
+    cout<<str<<endl;
+    str[0] = 'H';
+     cout<<str<<endl;
+    
+    
+}
---
+++ STRINGS/tempCodeRunnerFile.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main(){
+    string str = "Parteek";
+    cout<<str[0];
+    
+    
+}
