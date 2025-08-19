abb2adfabe134e04a1fc87c0ddbbc087059efc3f
marve01-hub <parteek.marve09@gmail.com>
2025-08-19 23:42:13 +0530

    library functions

---
+++ FUNCTIONS/libraryFunctions.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    // cout << sqrt(10);
+    // cout << cbrt(10); // library function
+    // cout << 2.15443 * 2.15443 * 2.15443;
+    // cout << max(4, 8);
+     // cout << min(4, 8);
+    cout << pow(2, 3);
+}
