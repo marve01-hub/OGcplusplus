dd459df3d0b7e775755613637349082afc8de076
marve01-hub <parteek.marve09@gmail.com>
2025-08-26 23:44:54 +0530

    CONSTRUCTOR CONCEPT

--- OOPS/Constructors.cpp
+++ OOPS/Constructors.cpp
@@ -5,9 +5,14 @@ public:
        string name;
        int rnumber;
        float cgpa;
-       int age;
-      
-       Student(string s, int r, float g){       //  CONSTRUCTOR***
+
+      Student(){
+// this is default constructor which is already present if we have not created constructor 
+// but if we have created a constructor then we have to make an empty construtor to initialize the
+// attributes if we want to initialize attributes by both ways  
+      }
+
+       Student(string s, int r, float g){       // PARAMETERISED CONSTRUCTOR***
             name = s;
             rnumber = r;
             cgpa = g;
@@ -16,6 +21,12 @@ public:
 
 int main() {
     Student s1("Parteek Marve",1330,8.5);
+
+    Student s2;
+    s2.name = "Parteek Marve";
+    s2.rnumber = 1398;
+    s2.cgpa = 8.3;
     
     cout<<s1.name<<" "<<s1.rnumber<<" "<<s1.cgpa<<endl;
+    cout<<s2.name<<" "<<s2.rnumber<<" "<<s2.cgpa<<endl;
 }
