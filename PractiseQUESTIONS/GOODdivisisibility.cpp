caa34aabf713acc4bbda95e137d88476e23bf890
marve01-hub <parteek.marve09@gmail.com>
2025-08-27 13:20:46 +0530

    to do

--- PractiseQUESTIONS/GOODdivisisibility.cpp
+++ PractiseQUESTIONS/GOODdivisisibility.cpp
@@ -1,25 +1,22 @@
 #include<iostream>
 using namespace std;
 
-// void fizzBuzz(int number) {
-//     int number 
-//     cin>>number;
-//     for(int i=0;i<=100;i++){
-//         if(number%3==0){
-//             return "Fizz";
-//         }
-//         if(number%5==0){
-//             return "Buzz";
-//         }
-//         if(number%3==0 and number%5==0)
-//         cout<<"FizzBuzz";
-//         }
+int main() {
+    int number;
+
+    cin>>number;
+    for(int i=0;i<=100;i++){
+        if(number%3==0){
+            cout<<"Fizz";
+            break;
+        }
+        if(number%5==0){
+            cout<<"Buzz";
+            break;
+        }
+        if(number%3==0 and number%5==0)
+        cout<<"FizzBuzz";
+        break;
+        }
         
-//     }
-int main(){
-    int a = 1;
-    int b = 2;
-    int c = 4;
-    int d = 8;
-    cout<<max(max(a,b),max(c,d));
-}
+    }
