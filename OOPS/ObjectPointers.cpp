1da17b8cafd24a3c9a90a9397f9f6209ccd52110
marve01-hub <parteek.marve09@gmail.com>
2025-08-29 00:09:34 +0530

    use of pointers to print and update

---
+++ OOPS/ObjectPointers.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+class Cricketer{
+public:
+    string name;
+    int runs;
+    float average;
+
+    Cricketer(string n, int r, float avg){
+       this->name = n;
+       this->runs = r;
+       this->average = avg;
+    }       
+};
+    void change(Cricketer* c){
+        c->runs = 54355;  // = (*c).run = 54355;
+    }
+           
+int main(){
+    Cricketer c1("Kl Rahul", 9000,45.2);
+    cout<<c1.runs<<endl;
+    change(&c1);
+    cout<<c1.runs<<endl;
+     
+    Cricketer* p1 = &c1; // p1 ke andar c1 ka address store hua jis se voh c1 ki values ko control kar skta hai = *p1 = c1
+    // cout<<(*p1).runs<<endl; // = (*c1).runs
+    // cout<<(*p1).runs<<endl; // changed value of runs
+    p1->runs = 18094; // (*p1).runs = 18094; - updation
+     cout<<p1->runs<<endl; // cout<<(*p1).runs<<endl; 
+}
+
+
+
+
+
