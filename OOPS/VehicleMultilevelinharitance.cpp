d637549204274710b484c38bc66c9caad2610ccf
marve01-hub <parteek.marve09@gmail.com>
2025-09-06 14:29:49 +0530

    MULTILEVEL + CINSTRUCTOR CHAINING

--- OOPS/VehicleMultilevelinharitance.cpp
+++ OOPS/VehicleMultilevelinharitance.cpp
@@ -1,5 +1,12 @@
 #include<iostream>
 using namespace std;
+
+/*!Vehicle (Base/Parent)
+    ↓
+TwoWheeler (Intermediate/Child) ──── FourWheeler (Intermediate/Child)
+    ↓                                      ↓
+Bike (Derived/GrandChild)              Car (Derived/GrandChild)*/ 
+
 class Vehicle {     // PARENT CLASS
 public: 
     string name;
@@ -21,23 +28,26 @@ public:
 
 class TwoWheeler : public Vehicle {   // CHILD CLASS   
 public:       
-    TwoWheeler(string name, float mileage, int topspeed, string fuelType) : Vehicle(name, mileage, topspeed, fuelType) {}  // Constructor chaining
+    TwoWheeler(string name, float mileage, int topspeed, string fuelType)
+     : Vehicle(name, mileage, topspeed, fuelType) {}  //! CONSTRUCTOR CHAINING
 };
 
 class FourWheeler : public Vehicle {  // CHILD CLASS   
 public:       
-    FourWheeler(string name, float mileage, int topspeed, string fuelType) : Vehicle(name, mileage, topspeed, fuelType) {}  // Constructor chaining
+    FourWheeler(string name, float mileage, int topspeed, string fuelType)
+     : Vehicle(name, mileage, topspeed, fuelType) {}  //! CONSTRUCTOR CHAINING
 };
 
 class Bike : public TwoWheeler {  // GRAND CHILD CLASS   
 public:        
-     Bike(string name,float mileage, int topspeed, string fuelType)
+     Bike(string name,float mileage, int topspeed, string fuelType) //! CONSTRUCTOR CHAINING
         : TwoWheeler(name, mileage, topspeed, fuelType) {}
 }; 
 
 class Car : public FourWheeler { // GRAND CHILD CLASS   
 public:        
-    Car(string name, float mileage, int topspeed, string fuelType) : FourWheeler(name, mileage, topspeed, fuelType) {}  // Constructor chaining
+    Car(string name, float mileage, int topspeed, string fuelType) 
+    : FourWheeler(name, mileage, topspeed, fuelType) {}  //! CONSTRUCTOR CHAINING
 };
 
 int main() {
