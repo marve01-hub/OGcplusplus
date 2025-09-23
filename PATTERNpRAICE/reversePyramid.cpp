6130e78469aa1806b1509415a77f26c200ccc106
marve01-hub <parteek.marve09@gmail.com>
2025-09-23 19:32:54 +0530

    Reverse

--- PATTERNpRAICE/reversePyramid.cpp
+++ PATTERNpRAICE/reversePyramid.cpp
@@ -6,17 +6,17 @@ int main(){
     cin>>n;
     for(int i = 0; i < n; i++) {
     // space
-    for(int j = 0; j < n - i - 1; j++) {
+    for(int j = 0; j < i ;j++) {
         cout << " ";
     }
 
     // star
-    for(int k = 0; k < 2 * i + 1; k++) {
+    for(int k = 0; k < 2*n - (2*i + 1); k++) {
         cout << "*";
     }
 
     // space
-    for(int l = 0; l < n - i - 1; l++) {
+    for(int l = 0; l < i; l++) {
         cout << " ";
     }
     cout<<endl;
