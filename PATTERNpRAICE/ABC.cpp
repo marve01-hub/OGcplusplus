96e4fffae7d3273df0717b3d964570b4d69edd14
marve01-hub <parteek.marve09@gmail.com>
2025-09-23 23:13:30 +0530

    ASCII VALUE vala concept

---
+++ PATTERNpRAICE/ABC.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter n : ";
+    cin>>n;
+
+    for(int i = 0;i <n;i++){
+        char ch = 'A' + i;  //! CONCEPT BOLTE*
+        for(int j = 0;j<=i;j++)
+        cout<<ch;
+        cout<<endl;
+    } 
+}
