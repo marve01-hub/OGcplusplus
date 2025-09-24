f0541ea5a17b9afdc74faf25d219f168f80bd52e
marve01-hub <parteek.marve09@gmail.com>
2025-09-24 19:43:39 +0530

    ***VERY GOOD CONCEPT/LOGIC

---
+++ PATTERNpRAICE/ABApyramid.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter n : ";
+    cin>>n;
+    for(int i = 0; i < n; i++) {
+    // space
+    for(int j = 0; j < n - i - 1; j++) {
+        cout << " ";
+    }
+
+    // characters
+    char ch = 'A';
+    int breakpoint = (2*i+1)/2;
+    for(int j= 1; j<=2*i+1;j++){
+            cout<<ch; 
+            if (j<= breakpoint) ch++;
+            else ch--;  
+        }
+      
+
+    // space
+    for(int l = 0; l < n - i - 1; l++) {
+        cout << " ";
+    }
+    cout<<endl;
+    }   
+}
+
+    
--- PATTERNpRAICE/ABC.cpp
+++ PATTERNpRAICE/ABC.cpp
@@ -6,7 +6,7 @@ int main(){
     cin>>n;
 
     for(int i = 0;i <n;i++){
-        char ch = 'A' + i;  //! CONCEPT BOLTE*
+        char ch = 'A' + i;  //! CONCEPT BOLTE*  in each new row new alphabet will come
         for(int j = 0;j<=i;j++)
         cout<<ch;
         cout<<endl;
