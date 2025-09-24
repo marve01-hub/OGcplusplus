93604576eecbd462d3238e712482f983f869c612
marve01-hub <parteek.marve09@gmail.com>
2025-09-25 00:44:31 +0530

    PAIRS

--- STL/BASICSofSTL.cpp
+++ STL/BASICSofSTL.cpp
@@ -10,13 +10,16 @@ int main() {
     cout << p.first << " " << p.second << endl; //! output
    */ 
 
-   // NESTED PAIRS(pair of pair)
+   //* NESTED PAIRS(pair of pair)
 
-   pair<int, pair<int, int>> p = {1,   {3,     4}};
+   /*pair<int, pair<int, int>> p = {1,   {3,     4}};
                            //     1st 2nd1st 2nd2nd      
-   cout << p.first << " " << p.second.first << " " << p.second.second << endl;
+   cout << p.first << " " << p.second.first << " " << p.second.second << endl;*/
 
+    //* ARRAY PAIRS
 
+    pair<int, int> arr[] = {{1, 2}, {3, 4}, {5, 6}};
+    cout << arr[1].second << endl;  //! output 4
     
 
 
