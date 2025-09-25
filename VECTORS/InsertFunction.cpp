5c85506c42ea2cbf7211f74635eba02925fec437
marve01-hub <parteek.marve09@gmail.com>
2025-09-25 18:51:03 +0530

    Inserting Lements In Vector

--- VECTORS/InsertFunction.cpp
+++ VECTORS/InsertFunction.cpp
@@ -3,13 +3,13 @@
 using namespace std;
 int main(){
 vector<int> v; 
-    v = {2,4,6,3,1};  // initial vector
-    v.insert(v.begin()+1,8); // to store single elements
+    v = {2,4,6};  // initial vector
+    v.insert(v.begin()+1,8); //! to store single elements =v.begin(POSITION,ELEMENT TO STORE)
     for(auto it = v.begin(); it != v.end(); it++) {
         cout<< *(it) << " ";
     }
     cout<<endl;
-     v.insert(v.begin()+1,2,5); // to store multiple elements =v.begin(POSITION,NO OF ELEMENTS,ELEMENT TO STORE)
+    v.insert(v.begin()+1,2,5); //! to store multiple elements =v.begin(POSITION,NO OF ELEMENTS,ELEMENT TO STORE)
     for(auto it = v.begin(); it != v.end(); it++) {
         cout<< *(it) << " ";
     }
