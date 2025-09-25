8a633b4ca178baffcde666d0fe07bcee71ba7095
marve01-hub <parteek.marve09@gmail.com>
2025-09-25 18:21:04 +0530

    ERASE Function

--- VECTORS/EraseFunction.cpp
+++ VECTORS/EraseFunction.cpp
@@ -4,7 +4,8 @@ using namespace std;
 int main(){
 vector<int> v; 
     v = {2,4,6,3,1};  // initial vector
-    v.erase(v.begin()+1, v.begin()+3);
+ //!   v.erase(address from starting, address from ending)
+    v.erase(v.begin()+1, v.begin()+3);  //* erases elements from index 1 to index 3-1 = 2
     for(auto it = v.begin(); it != v.end(); it++) {
         cout<< *(it) << " ";
     }
