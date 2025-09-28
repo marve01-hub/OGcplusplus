12b9c72342352a8b45adcc23f7d9f4637e1983ca
marve01-hub <parteek.marve09@gmail.com>
2025-09-28 23:37:55 +0530

    Divisor printing

---
+++ BasicMathematics/printallDivisorsOfN.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main(){
+    int n;
+    cout<<"enter n: ";
+    cin>>n;
+    for(int i = 1;i<=n;i++){
+        if(n%i==0) cout<<i<<" ";
+    }
+}
