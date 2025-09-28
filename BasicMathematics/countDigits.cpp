8791816282240939a2eee29fea347e6e9e3fef7b
marve01-hub <parteek.marve09@gmail.com>
2025-09-28 20:07:21 +0530

    TC and Log concept

--- BasicMathematics/countDigits.cpp
+++ BasicMathematics/countDigits.cpp
@@ -1,14 +1,23 @@
 #include<iostream>
+#include<cmath>
 using namespace std;
 int main(){
     int n;
     cout<<"enter n: ";
     cin>>n;
-    int cnt = 0;
-    while(n>0){
-        int last_digit = n % 10;
-        cnt = cnt + 1;
-        n = n/10;
-    }
-     cout<<cnt;
+    // int cnt = 0;
+
+    //! *Method by Log
+    int cnt = (int)(log10(n)) + 1; //!time complexity when number of iterations depend upon division then TC is logarithmic
+    cout<<cnt;
+
+
+    // while(n>0){
+    //     int last_digit = n % 10;
+    //     cnt = cnt + 1;
+    //     n = n/10;
+    // }
+    //  cout<<cnt;
+
+
 }
