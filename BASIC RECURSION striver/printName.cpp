6502a4a6fe9ed45183aaf8e002f111cef1255f24
marve01-hub <parteek.marve09@gmail.com>
2025-09-30 00:25:47 +0530

    Basic Concept

--- BASIC RECURSION striver/printName.cpp
+++ BASIC RECURSION striver/printName.cpp
@@ -1,17 +1,17 @@
 #include<iostream>
 using namespace std;
-void greet(int i, int n){
-    if(i>n) return;
-    cout<<"Hello"<<endl;
+void greet(int n, int i){
+    if(n<i) return;
+    cout<<n<<endl;
     
     
     
-     greet(i+1,n); //!if we don't increase value of i then base condition will remain unused and will run till infinity and stack overflow happens
+     greet(n-1,i); 
      
    
 }
 int main(){
     int n;
     cin>>n;
-    greet(1,n);
+    greet(n,1);
 }
