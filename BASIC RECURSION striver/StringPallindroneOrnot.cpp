32ae13d103f55f0d6c41e70fd13edce2771cc30c
marve01-hub <parteek.marve09@gmail.com>
2025-10-07 19:19:36 +0530

    String Ka Concept

--- BASIC RECURSION striver/StringPallindroneOrnot.cpp
+++ BASIC RECURSION striver/StringPallindroneOrnot.cpp
@@ -3,7 +3,7 @@ using namespace std;
 #include<algorithm>
 bool pal(int i, string &s){
     
-    if(i>=s.size()/2) return true; 
+    if(i>=s.size()/2) return true; //! agar hum index 0 se travel karte huye middle tak pohonch gaye to matlab sab ok ok hai
     if(s[i]!=s[s.size()-i-1]) return false;
     pal(i+1,s);
 } 
