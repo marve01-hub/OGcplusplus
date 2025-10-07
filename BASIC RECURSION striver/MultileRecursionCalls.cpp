06486a5dffac0e30677f5134955758685e97f0c1
marve01-hub <parteek.marve09@gmail.com>
2025-10-07 20:28:09 +0530

    MULTIPLE RECURSION CALL via fibancii number

---
+++ BASIC RECURSION striver/MultileRecursionCalls.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int fib(int n){
+    if(n<=1) return 1;
+    
+    int last = fib(n-1);
+    int slast = fib(n-2);
+
+    return last + slast;
+}
+
+int main(){
+    int n;
+    cout<<"enter n: ";
+    cin>>n;
+    cout<<fib(n);
+}
